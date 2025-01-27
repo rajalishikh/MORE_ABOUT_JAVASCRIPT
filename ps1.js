3fdac5d148c15f9b6ae938d356e0bc9f06993b81
rajalishikhe <nahidujjamanraj@gmail.com>
2025-01-27 12:04:06 +0600

    I write a function that display a sms after 5 seconds

---
+++ ps1.js
@@ -0,0 +1,8 @@
+// 1a) Write a function that displays a message after 5s.
+function display_sms(){
+    let a=setTimeout(()=>{
+        console.log("Success is not come ober  a night")
+    },5000)
+    return a
+}
+display_sms()

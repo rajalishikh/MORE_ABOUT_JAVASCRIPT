52f746164d979b558b5e700ec2ee724809e00d0d
rajalishikhe <nahidujjamanraj@gmail.com>
2025-02-17 08:37:42 +0600

    I solve some basic problem

---
+++ ps11.js
@@ -0,0 +1,22 @@
+// problem 1 
+const num=12
+const text='My wife all ways angry'
+console.log(typeof num,typeof text)
+// problem 2 
+let strNum="123"
+let Num=parseInt(strNum)
+console.log("problem 2 ",typeof Num)
+// problem 3
+const firstName="Md Raj"
+const lastName="Ali Sheikh"
+let fullName=firstName+" "+ lastName
+// console.log(fullName)
+
+let greeting=`Hello my name is ${fullName}`
+// console.log(greeting)
+
+let isJavaScriptFun=true
+console.log("is javaScript fun",isJavaScriptFun)
+let unknown;
+let empty = null;
+console.log(unknown,typeof unknown,empty,typeof empty)

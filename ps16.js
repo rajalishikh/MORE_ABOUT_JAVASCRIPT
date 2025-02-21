c5dfad34f0246daa83aa1108c99e7b207fa41ae5
rajalishikhe <nahidujjamanraj@gmail.com>
2025-02-21 22:09:42 +0600

    I sove a basic problem hacker Ranks

---
+++ ps16.js
@@ -0,0 +1,12 @@
+// Implement a function named factorial that has one parameter: an integer, . It must return the value of (i.e., factorial).
+
+function factorial(number){
+    let num=1
+for(let x=1;x<=number;x++){
+    
+    num=num*x;
+}
+console.log(num)
+
+}
+factorial(4)
